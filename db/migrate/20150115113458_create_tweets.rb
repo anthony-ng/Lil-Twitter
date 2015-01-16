a563c8b755be0b619da5ea97332cb99fe6b22806
Anthony Ng & Daniel Kelley & jeremymcintyre & Leonard Bui <pair+anthony-ng.danimalkelley.jeremymcintyre.lnbcoding@devbootcamp.com>
2015-01-15 16:09:07 -0800

    Added new form for new tweet; Updated file paths for erb references

--- db/migrate/20150115113458_create_tweets.rb
+++ db/migrate/20150115113458_create_tweets.rb
@@ -1,11 +1,11 @@
 class CreateTweets < ActiveRecord::Migration
   def change
     create_table :tweets do |t|
-      t.belongs_to :category
-      t.string :title
+      t.belongs_to :user
       t.string :content
 
       t.timestamps
     end
   end
 end
+
